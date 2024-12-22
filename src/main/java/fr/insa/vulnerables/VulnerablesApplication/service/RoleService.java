cf42386d8d46dc898d89e52955bffdcd481da9bc
Sven Leko <82047732+sleko01@users.noreply.github.com>
2024-12-22 12:13:09 +0100

    Delete src directory

--- src/main/java/fr/insa/vulnerables/VulnerablesApplication/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package fr.insa.vulnerables.VulnerablesApplication.service;
-
-import fr.insa.vulnerables.VulnerablesApplication.domain.Role;
-
-import java.util.List;
-
-public interface RoleService {
-    List<Role> getAllRoles();
-}
